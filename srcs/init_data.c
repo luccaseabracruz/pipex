d2e3290c67ae07fa61dee9873b262496e9f577b0
Lucca Cruz <lucca1609.ls@gmail.com>
2025-08-19 17:52:41 +0100

    fix: macro name.

--- srcs/init_data.c
+++ srcs/init_data.c
@@ -6,7 +6,7 @@
 /*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 15:28:32 by lseabra-          #+#    #+#             */
-/*   Updated: 2025/08/18 15:58:09 by lseabra-         ###   ########.fr       */
+/*   Updated: 2025/08/19 17:52:21 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	init_data(t_pipex *data, char **argv, char **envp)
 	if (data->fds[0] < 0)
 	{
 		dup2(STDERR_FILENO, STDOUT_FILENO);
-		ft_printf("%s: %s: %s\n", PROGRAM_NAME, NO_FILE_OR_DIR_MSG, argv[1]);
+		ft_printf("%s: %s: %s\n", PROGRAM_NAME, NO_FILE_MSG, argv[1]);
 	}
 	data->fds[1] = open(argv[4], (O_WRONLY | O_CREAT | O_TRUNC), 0644);
 	if (data->fds[1] < 0)
